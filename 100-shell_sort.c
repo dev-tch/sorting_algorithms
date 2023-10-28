3e987daf644b316f0891161696e0cd959254f2f4
dev-tch <tijeni.chaabeni@gmail.com>
2023-10-28 13:01:22 +0000

    fix shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -13,7 +13,7 @@ void shell_sort(int *array, size_t size)
 	int newElem;
 
 	/*check validity of data*/
-	if (!array || size <= 0)
+	if (!array || size < 1)
 		return;
 
 	/* calculate the Knuth sequence*/
